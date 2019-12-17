cf41731556906e8dcff06525dd9f3d6606cc5e7d
Dexter Roussel <dexterleetcb@gmail.com>
2019-12-17 16:16:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,7 @@
-# Add your variables here
-# Add your variables here 
+first_number=get_variable_from_file.do
+filter_parameter_logging
+expect(first_number).to be_an(Integer).or be_a(Float)
+filter_parameter_logging
+'learn'
+'learn'
+.learn
